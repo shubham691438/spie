91663f1b19a3ccf515f18002faa29727fbc47709
mkv-mahi <2023ugpi019@nitjsr.ac.in>
2025-01-02 20:30:08 +0530

    Update Blog.jsx
    blog page

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,1 +1,172 @@
-
+import React from 'react';
+  const Blog = () => {
+        return (
+          <div id="body" className="bg-gray-900  text-white">
+            <header id="first" className="flex justify-between items-center">
+              <p
+                id="blog"
+                className="ml-4 m-9 text-6xl text-white uppercase animate-glitcher font-serif"
+              >
+                blogs
+              </p>
+              <nav id="nav" className="flex justify-end items-end h-20 w-36 rounded-2xl">
+                <img
+                  id="menu"
+                  src="menu.png"
+                  alt="Menu icon"
+                  className="h-10 w-10 m-2 mt-5 mr-12"
+                />
+              </nav>
+            </header>
+            <main id="main-content">
+            <section id="main" className="h-36 ml-5 mb-6 flex flex-col bg-gray bg-[length:12px_13px] bg-[linear-gradient(to_right,#212121_1px,transparent_1px),linear-gradient(to_bottom,#424242_1px,transparent_1px)] border border-white border-groove rounded-xl ">
+                <article id="pdiv" className="flex justify-between items-center w-full mt-2 ml-2">
+                  <a
+                    id="link1"
+                    href="..."
+                    className="flex justify-between items-center w-full bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-blue-400  text-3xl ml-1 font-serif no-underline uppercase hover:underline hover:decoration-2  hover:decoration-cyan-400"
+                    >
+                    placement experience
+                  </a>
+                </article>
+                <div id="main2" className="flex justify-between items-end">
+                  <div id="main3" className="flex justify-start items-center ml-2">
+                    <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #Core
+                    </div>
+                    <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif ">
+                      #Non-core
+                    </div>
+                  </div>
+                  <p id="link2" className="flex justify-between items-end">
+                    <img
+                      id="spic"
+                      src="menu.png"
+                      alt="Menu icon"
+                      className="h-10 w-10 rounded-full mr-7 mb-4"
+                    />
+                  </p>
+                </div>
+              </section>
+              <section id="main" className="h-36 ml-5 mb-6 flex flex-col bg-gray bg-[length:12px_13px] bg-[linear-gradient(to_right,#212121_1px,transparent_1px),linear-gradient(to_bottom,#424242_1px,transparent_1px)] border border-white border-groove rounded-xl ">
+                <article id="pdiv" className="flex justify-between items-center w-full mt-2 ml-2">
+                  <a
+                    id="link1"
+                    href="......."
+                    className="flex justify-between items-center w-full bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-blue-400  text-3xl ml-1 font-serif no-underline uppercase hover:underline hover:decoration-2  hover:decoration-cyan-400"
+                    >
+                    fun events experiences
+                  </a>
+                </article>
+                <div id="main2" className="flex justify-between items-end">
+                  <div id="main3" className="flex justify-start items-center ml-2">
+                  <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #proff.
+                    </div>
+                    <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #students
+                    </div>
+                  </div>
+                  <p id="link2" className="flex justify-between items-end">
+                    <img
+                      id="spic"
+                      src="menu.png"
+                      alt="Menu icon"
+                      className="h-10 w-10 rounded-full mr-7 mb-4"
+                    />
+                  </p>
+                </div>
+              </section>
+              <section id="main" className="h-36 ml-5 mb-6 flex flex-col bg-gray bg-[length:12px_13px] bg-[linear-gradient(to_right,#212121_1px,transparent_1px),linear-gradient(to_bottom,#424242_1px,transparent_1px)] border border-white border-groove rounded-xl ">
+                <article id="pdiv" className="flex justify-between items-center w-full mt-2 ml-2">
+                  <a
+                    id="link1"
+                    href="......."
+                    className="flex justify-between items-center w-full bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-blue-400  text-3xl ml-1 font-serif no-underline uppercase hover:underline hover:decoration-2  hover:decoration-cyan-400"
+                    >
+                    seniors advice
+                  </a>
+                </article>
+                <div id="main2" className="flex justify-between items-end">
+                  <div id="main3" className="flex justify-start items-center ml-2">
+                  <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #1st year
+                    </div>
+                    <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #2nd year
+                    </div>
+                  </div>
+                  <p id="link2" className="flex justify-between items-end">
+                    <img
+                      id="spic"
+                      src="menu.png"
+                      alt="Menu icon"
+                      className="h-10 w-10 rounded-full mr-7 mb-4"
+                    />
+                  </p>
+                </div>
+              </section>
+              <section id="main" className="h-36 ml-5 mb-6 flex flex-col bg-gray bg-[length:12px_13px] bg-[linear-gradient(to_right,#212121_1px,transparent_1px),linear-gradient(to_bottom,#424242_1px,transparent_1px)] border border-white border-groove rounded-xl ">
+                <article id="pdiv" className="flex justify-between items-center w-full mt-2 ml-2">
+                  <a
+                    id="link1"
+                    href="....."
+                    className="flex justify-between items-center w-full bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-blue-400  text-3xl ml-1 font-serif no-underline uppercase hover:underline hover:decoration-2  hover:decoration-cyan-400"
+                    >
+                    subject related
+                  </a>
+                </article>
+                <div id="main2" className="flex justify-between items-end">
+                  <div id="main3" className="flex justify-start items-center ml-2">
+                  <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #pie
+                    </div>
+                    <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #core
+                    </div>
+                  </div>
+                  <p id="link2" className="flex justify-between items-end">
+                    <img
+                      id="spic"
+                      src="menu.png"
+                      alt="Menu icon"
+                      className="h-10 w-10 rounded-full mr-7 mb-4"
+                    />
+                  </p>
+                </div>
+              </section>
+              <section id="main" className="h-36 ml-5 mb-6 flex flex-col bg-gray bg-[length:12px_13px] bg-[linear-gradient(to_right,#212121_1px,transparent_1px),linear-gradient(to_bottom,#424242_1px,transparent_1px)] border border-white border-groove rounded-xl ">
+                <article id="pdiv" className="flex justify-between items-center w-full mt-2 ml-2">
+                  <a
+                    id="link1"
+                    href="https://www.google.com"
+                    className="flex justify-between items-center w-full bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-blue-400  text-3xl ml-1 font-serif no-underline uppercase hover:underline hover:decoration-2  hover:decoration-cyan-400"
+                    >
+                    gate study materials
+                  </a>
+                </article>
+                <div id="main2" className="flex justify-between items-end">
+                  <div id="main3" className="flex justify-start items-center ml-2">
+                  <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #production
+                    </div>
+                    <div className="text-white text-2xl m-6 mt-5 ml-2 flex justify-center items-center h-8 w-30 rounded-lg bg-gray-500 capitalize font-serif">
+                      #books
+                    </div>
+                  </div>
+                  <p id="link2" className="flex justify-between items-end">
+                    <img
+                      id="spic"
+                      src="menu.png"
+                      alt="Menu icon"
+                      className="h-10 w-10 rounded-full mr-7 mb-4"
+                    />
+                  </p>
+                </div>
+              </section>
+            </main>
+          </div>
+        );
+      };
+  export default Blog;
+  
