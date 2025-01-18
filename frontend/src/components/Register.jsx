13eca9de9fa0492df5720bc95f9d35f00ba5d81e
Vikash Kumar <145967694+RealityDenied@users.noreply.github.com>
2025-01-18 11:24:36 +0530

    Update Register.jsx removed styling for iframe

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -15,34 +15,12 @@ const Register = () => {
     return () => window.removeEventListener("load", handlePageLoad);
   }, []);
 
-  const iframeWrapper1Style = {
-    filter: "invert(1)"
-  };
-
-  const iframeWrapper2Style = {
-    filter: "hue-rotate(224.18deg) saturate(62.87%) brightness(108.04%)"
-  };
-
-  const loaderStyle = {
-    width: "50px",
-    height: "50px",
-    border: "5px solid #f3f3f3",
-    borderTop: "5px solid #3498db",
-    borderRadius: "50%",
-    animation: "spin 2s linear infinite"
-  };
-
-  const spinKeyframes = `@keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }`;
-
   return (
     <>
       {/* Loader */}
       {isLoading ? (
         <div className="fixed inset-0 flex justify-center items-center bg-black text-white z-50">
-          <div style={loaderStyle}></div>
+          <p>Loading...</p>
         </div>
       ) : (
         <div className="flex flex-col bg-gray-900 text-white">
@@ -53,21 +31,18 @@ const Register = () => {
 
           {/* Main Content (Iframe container) */}
           <main className="flex-1 bg-gray-900 p-4">
-            <div style={iframeWrapper1Style}>
-              <div style={iframeWrapper2Style}>
-                <iframe
-                  src="https://docs.google.com/forms/d/e/1FAIpQLScbEY-q6R71VkyCMUmR-dMTUVdf_fxYD1637Gv94pIqcucZWQ/viewform?embedded=true"
-                  width="100%"
-                  height="2283"
-                  frameBorder="0"
-                  marginHeight="0"
-                  marginWidth="0"
-                  title="Google Form"
-                  className="w-full"
-                >
-                  Loading…
-                </iframe>
-              </div>
+            <div>
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLScbEY-q6R71VkyCMUmR-dMTUVdf_fxYD1637Gv94pIqcucZWQ/viewform?embedded=true"
+                width="100%"
+                height="2283"
+                frameBorder="0"
+                marginHeight="0"
+                marginWidth="0"
+                title="Prodyog Registration Form"
+              >
+                Loading…
+              </iframe>
             </div>
           </main>
 
@@ -77,9 +52,6 @@ const Register = () => {
           </footer>
         </div>
       )}
-
-      {/* Add the spin animation styles dynamically */}
-      <style>{spinKeyframes}</style>
     </>
   );
 };
